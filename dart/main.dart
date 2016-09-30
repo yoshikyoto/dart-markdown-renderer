e3232348343af4ad2adb25ad87faa0835a87a817
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2016-09-30 21:58:17 +0900

    submodule消した

--- dart/main.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:html';
-import 'dart:async';
-import 'Controller.dart';
-
-void main() {
-   Controller controller = new Controller();
-   new Timer.periodic(const Duration(seconds: 1), controller.periodic);
-}
